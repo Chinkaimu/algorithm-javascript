9523953c6fe47e323fb2277788ddf12dad64787b
chinkaimu <823388255@qq.com>
2020-01-22 14:52:59 +0800

    feat: binary search

---
+++ search/binarySearch.js
@@ -0,0 +1,45 @@
+/**
+ * @param {*} array 
+ * @param {*} target 
+ */
+function binarySearch(array, target) {
+  if (array.length === 0) {
+    return [-1,-1];
+  }
+
+  let start = 0;
+  let end = array.length - 1;
+  let mid;
+
+  while (start+1 < end) {
+    mid = parseInt(start + (end - start)/2);
+    if (array[mid] === target) {
+      /** Can't stop while find the value, cause it maybe not the first position. */
+      /** If we want to find the last position, we should use start here. */
+      end = mid;
+    } else if (array[mid] < target) {
+      start = mid;
+    } else if (array[mid] > target) {
+      end = mid;
+    }
+  }
+
+  /**
+   * The first position is the start value or the end value.
+   */
+  if (array[start] === target) {
+    return start;
+  }
+
+  /**
+   * It's also find the first position not the last position.
+   */
+  if (array[end] === target) {
+    return end;
+  }
+
+  return -1;
+}
+
+console.log(binarySearch([0,1,1],1))
+console.log(binarySearch([5,7,8,8,8,10],8))
---
+++ search/searchPosition.js
@@ -0,0 +1,65 @@
+/**
+ * Given a sorted array of n integers, find the starting and ending 
+ * position of a given target value. If the target is not found in
+ * array, return [-1,-1]
+ * Example given [5,7,7,8,8,10] and target value 8, return [3,4]
+ * @param {*} array 
+ * @param {*} target 
+ */
+function searchPosition(array, target) {
+  if (array.length === 0) {
+    return [-1,-1];
+  }
+
+  let start = 0;
+  /** initial value is 5 */
+  let end = array.length - 1;
+  let mid;
+
+  while (start+1 < end) {
+    /**initial value is 2*/
+    mid = parseInt(start + (end - start)/2);
+    if (array[mid] === target) {
+      end = mid;
+    } else if (array[mid] < target) {
+      start = mid;
+    } else if (array[mid] > target) {
+      end = mid;
+    }
+  }
+
+  if (array[start] === target) {
+    end = start;
+    for (let i = start+1; i < array.length; i++) {
+      if (array[i] === target) {
+        end = i;
+      } else if (array[i] !== target) {
+        return [start, end];
+      }
+    }
+  }
+
+  if (array[end] === target) {
+    start = end;
+    for (let i = end+1; i < array.length; i++) {
+      if (array[i] === target) {
+        end = i;
+        // if we check the last position, return
+        if (i === array.length - 1) {
+          return [start, end];
+        }
+        // if we find the unequal value , return
+      } else if (array[i] !== target) {
+        return [start, end];
+      }
+    }
+  }
+
+  return [-1, -1];
+}
+
+console.log(searchPosition([],9))
+console.log(searchPosition([0,1,1],1))
+console.log(searchPosition([1,1,1,1,2,3],1))
+console.log(searchPosition([5,7,7,8,8,8,8,10],8))
+console.log(searchPosition([5,7,7,8,8,8,8,10],11))
