29ba4f5c80ac1415f60436ab88c19226d39e1709
chinkaimu <823388255@qq.com>
2020-01-26 10:37:49 +0800

    fix: 74 search a 2D matrix

--- leetcode1/74SearchA2DMatrix.js
+++ leetcode1/74SearchA2DMatrix.js
@@ -6,27 +6,37 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-  var lastLen = matrix[matrix.length-1].length;
-  if (matrix.length === 0 || target < matrix[0][0] || target > matrix[matrix.length-1][lastLen]) {
+  if (matrix.length === 0) {
     return false;
   }
 
-  return searchTarget(searchRow(matrix, target));
+  if (Array.isArray(matrix[0]) && target < matrix[0][0]) {
+    return false;
+  }
+
+  if (Array.isArray(matrix[matrix.length-1])) {
+    var len = matrix[matrix.length - 1].length;
+    if (target > matrix[matrix.length - 1][len]) {
+      return false;
+    }
+  }
+
+  var row = searchRow(matrix, target);
+  if (typeof row === 'boolean') {
+    return row;
+  }
+  return searchTarget(matrix[row], target);
 };
 
 var searchTarget = function (array, target) {
-  
-}
-var searchRow = function (array, target) {
   var start = 0;
-  var end = array.length;
+  var end = array.length - 1;
   var mid;
 
-  // While start and end is adjacent or equal, it should break out.
   while (start + 1 < end) {
     mid = parseInt(start + (end - start)/2);
     if (array[mid] === target) {
-      return mid;
+      return true;
     } else if (array[mid] < target) {
       start = mid;
     } else if (array[mid] > target) {
@@ -34,13 +44,58 @@ var searchRow = function (array, target) {
     }
   }
 
-  if (array[start] >= target && start > 1) {
+  if (array[start] === target || array[end] === target) {
+    return true;
+  }
+
+  return false;
+}
+
+var searchRow = function (array, target) {
+  var start = 0;
+  var end = array.length - 1;
+  var mid;
+
+  // While start and end is adjacent or equal, it should break out.
+  while (start + 1 < end) {
+    mid = parseInt(start + (end - start)/2);
+    if (array[mid][0] === target) {
+      return true;
+    } else if (array[mid][0] < target) {
+      start = mid;
+    } else if (array[mid][0] > target) {
+      end = mid;
+    }
+  }
+
+  if (array[start] === target) {
+    return true;
+  }
+
+  if (array[end] === target) {
+    return true;
+  }
+
+  if (array[start][0] > target && start >= 1) {
     return start - 1;
   }
 
-  if (array[end] >= target && end > 1) {
+  if (array[end][0] > target && end >= 1) {
     return end - 1;
   }
 
   return array.length - 1;
 }
+
+var matrix1 = [
+  [1, 3, 5, 7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+];
+console.log(searchMatrix(matrix1, 0));
+console.log(searchMatrix(matrix1, 1));
+console.log(searchMatrix(matrix1, 10));
+console.log(searchMatrix(matrix1, 23));
+console.log(searchMatrix(matrix1, 11));
+console.log(searchMatrix(matrix1, 50));
+
