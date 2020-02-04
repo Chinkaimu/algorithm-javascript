0f1743c571b83b2eab7c16c297d786484012cbb0
chinkaimu <823388255@qq.com>
2020-02-04 16:11:10 +0800

    fix: modify the initial of maxMountKey

--- sort/assignedSort.js
+++ sort/assignedSort.js
@@ -4,32 +4,28 @@ let newArray1 = ["B","D","A","C"]
 let newArray2 = ["C","D","A","B"]
 let newArray3 = ["D","C","B","A"]
 let newArray4 = ["D","E","B","A"]
+let newArray5 = ["F", "D","E","B","A"]
 
 function assignedSorted(oldArray, newArray) {
-  let maxMountedIndex = 0
+  let maxMountIndex = -1
 
   for (let i = 0; i < newArray.length; i++) {
     let oldMountKey = getKeyFromAnother(newArray[i], oldArray)
 
     // 在后面插入新节点需要插入，插入节点数+1
     if (oldMountKey === -1) {
-      oldArray.splice(maxMountedIndex+1, 0, newArray[i])
+      oldArray.splice(maxMountIndex + 1, 0, newArray[i])
     }
 
-    if (oldMountKey < maxMountedIndex) {
-      // 从 oldMountKey 移动到 maxMountedIndex
-      // moveChild(oldMountKey, maxMountedIndex)
-      // console.log(oldArray.splice(oldMountKey,1))
-      // console.log('oldArray1', oldArray)
-      // console.log(oldArray.splice(maxMountedIndex,0,newArray[i]))
-      // console.log('oldArray2', oldArray)
+    if (oldMountKey < maxMountIndex) {
+      // 从 oldMountKey 移动到 maxMountIndex
       const toRemoveItem = oldArray.splice(oldMountKey,1)
-      oldArray.splice(maxMountedIndex,0,toRemoveItem[0])
+      oldArray.splice(maxMountIndex,0,toRemoveItem[0])
     }
 
-    if (oldMountKey > maxMountedIndex) {
-      maxMountedIndex = oldMountKey
-      // console.log('maxMountedIndex',maxMountedIndex)
+    if (oldMountKey > maxMountIndex) {
+      maxMountIndex = oldMountKey
+      // console.log('maxMountIndex',maxMountIndex)
     }
   }
 
@@ -55,10 +51,19 @@ function getKeyFromAnother(value, array) {
   return -1
 }
 
-console.log(assignedSorted(oldArray, newArray))
-console.log(assignedSorted(oldArray, newArray1))
-console.log(assignedSorted(oldArray, newArray2))
-console.log(assignedSorted(oldArray, newArray3))
-console.log(assignedSorted(oldArray, newArray4))
+console.log(equalArray(assignedSorted(oldArray, newArray), newArray))
+console.log(equalArray(assignedSorted(oldArray, newArray1), newArray1))
+console.log(equalArray(assignedSorted(oldArray, newArray2), newArray2))
+console.log(equalArray(assignedSorted(oldArray, newArray3), newArray3))
+console.log(equalArray(assignedSorted(oldArray, newArray4), newArray4))
+console.log(equalArray(assignedSorted(oldArray, newArray5),newArray5))
 
-
+function equalArray (arr1, arr2) {
+  if (arr1.length !== arr2.length) return false
+  for (let i in arr1) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+  return true
+}
