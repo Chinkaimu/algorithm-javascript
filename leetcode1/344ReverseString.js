4db0d5123a129f025fb5ea47505dbd21013900c2
chinkaimu <823388255@qq.com>
2020-02-10 18:53:47 +0800

    feat: 34 reverse string

---
+++ leetcode1/344ReverseString.js
@@ -0,0 +1,26 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+  let flag = false
+  if (!Array.isArray(s)) {
+    s = s.split('')
+    flag = true
+  }
+  let start = 0
+  let end = s.length - 1
+
+  while (start < end) {
+    const temp = s[start]
+    s[start] = s[end]
+    s[end] = temp
+    ++start
+    --end
+  }
+
+  return flag ? s.join('') : s
+};
+
+console.log(reverseString(["h","e","l","l","o"]))
+console.log(reverseString("hello"))
