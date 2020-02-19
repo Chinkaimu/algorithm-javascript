e5dec81a85a78b27a881f6ff2309a0baf22486b5
chinkaimu <823388255@qq.com>
2020-02-19 21:09:53 +0800

    feat: solution for 132 palindrome partition II

--- class4-DynamicProgramming/132PalindromePartitionII.js
+++ class4-DynamicProgramming/132PalindromePartitionII.js
@@ -7,20 +7,56 @@
 var minCut = function(s) {
   if (!s || s.length <= 1) return 0
 
-  const cuts = [0]
-  const palindrome = [[true]]
+  const palindrome = getPalindrome(s)
+  if (palindrome[0][s.length - 1]) return 0
 
-  palindrome[1][1] = true
-  if (s[1] === s[0]) {
-    palindrome[0][1] = true
-    cuts[1] = cuts[0] + 0
-  } else {
-    palindrome[0][1] = false
-    cuts[1] = cuts[0] + 1
+  // initial
+  const cuts = [0]
+  for (let i = 1; i < s.length; i++) {
+    if (palindrome[0][i]) {
+      cuts[i] = 0
+      continue
+    } else {
+      cuts[i] = i
+    }
+
+    for (let j = 0; j < i; j++) {
+      if (palindrome[j + 1][i]) {
+        cuts[i] = Math.min(cuts[i], cuts[j] + 1)
+      }
+    }
   }
 
-  for (let i = 2; i < s.length; i++) {
+  return cuts[s.length - 1]
+};
+
+function getPalindrome (s) {
+  palindrome = []
+
+  for (let i = 0; i < s.length; i++) {
+    palindrome[i] = []
     palindrome[i][i] = true
 
+    if (i + 1 < s.length) {
+      palindrome[i][i + 1] = s[i] === s[i + 1]
+    }
   }
-};
+
+  // o(n^2)
+  for (let step = 2; step < s.length; step++) {
+    for (let i = 0; i + step < s.length; i++) {
+      palindrome[i][i + step] = palindrome[i + 1][i + step - 1] && s[i] === s[i + step]
+    }
+  }
+
+  return palindrome
+}
+
+// Testcase
+console.log(minCut(''))
+console.log(minCut('aab'))
+console.log(minCut('abc'))
+console.log(minCut('abccba'))
+console.log(minCut('abacabab'))
+
+
