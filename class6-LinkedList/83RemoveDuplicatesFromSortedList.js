86a2c69742ed01e5c5207e386e5af712c52bcf9e
chinkaimu <823388255@qq.com>
2020-02-21 21:46:11 +0800

    feat: solution for 83

---
+++ class6-LinkedList/83RemoveDuplicatesFromSortedList.js
@@ -0,0 +1,48 @@
+/**
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+  if (!head || !head.next) return head
+
+  let fast = head.next
+  let slow = head
+
+  
+  while (fast !== null) {
+    while (fast.val === slow.val) {
+      fast = fast.next
+      slow.next = fast
+      // Important: 可能 fast 走到最后了
+      if (fast === null) {
+        return head
+      }
+    }
+    slow = fast
+    fast = fast.next
+  }
+
+  return head
+};
+
+// Testcase
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+const head = new ListNode(1)
+const node1 = new ListNode(1)
+const node2 = new ListNode(2)
+
+head.next = node1
+node1.next = node2
+
+console.log(deleteDuplicates(head))
