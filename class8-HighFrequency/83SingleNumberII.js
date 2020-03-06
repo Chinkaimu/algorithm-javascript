8481ea68d442123c10547478e2892e5fd3ccc6fd
chinkaimu <823388255@qq.com>
2020-03-06 23:07:40 +0800

    feat: solution for 137

--- class8-HighFrequency/83SingleNumberII.js
+++
@@ -1,7 +0,0 @@
-/**
- * @param A: An integer array
- * @return: An integer
- */
-// 思路：巧妙地使用
-const singleNumberII = function (A) {
-}
---
+++ class8-HighFrequency/BitManipulation-137SingleNumberII.js
@@ -0,0 +1,15 @@
+/**
+ * https://leetcode.com/problems/single-number-ii/
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {}
+  
+// a b;
+// 00 1 => 01;
+// 01 1 => 10;
+// 10 1 => 00;
+// 00 0 => 00;
+// 01 0 => 01;
+// 10 0 => 10;
+};
