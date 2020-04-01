0d41fe87a6e65a57dff6550de170e7c3b22a70c7
chinkaimu <823388255@qq.com>
2020-04-02 03:16:48 +0800

    fix: wrong answer for 215

--- type3-Sort/215KthLargestElementInAnArray.js
+++ type3-Sort/215KthLargestElementInAnArray.js
@@ -12,12 +12,16 @@ var findKthLargest = function(nums, k) {
   while (true) {
     let left = start;
     let right = end;
-    let mid = Math.floor((left + right)/2);
-
+    let pivot = nums[Math.floor((left + right)/2)];
+  
     while (left <= right) {
-      let pivot = nums[mid];
-      nums[left] > pivot && left++;
-      nums[right] < pivot && right--;
+      while (nums[left] > pivot) {
+        left++;
+      }
+
+      while (nums[right] < pivot) {
+        right--;
+      }
 
       if (left <= right) {
         left < right && ([nums[left], nums[right]] = [nums[right], nums[left]]);
@@ -25,17 +29,19 @@ var findKthLargest = function(nums, k) {
         right--;
       }
     }
-    console.log(nums);
-    console.log(left);
-    // left 左边的数都比它大
-    if ((k - 1) === left) {
-      return nums[left];
+
+    // left 有可能超出界限
+    // 2 种指针存在情况： right pivotIndex left; right left(left 可能指向原来的 pivot)
+    if (right + 1 === k - 1) {
+      return nums[right + 1]
     } else if ((k - 1) < left) {
-      end = left;
+      end = right;
     } else {
       start = left;
     }
   }
 }
 
-console.log(findKthLargest([3,2,1,5,6,4], 2));
+console.log(findKthLargest([-1,2,0], 2));  // 0
+console.log(findKthLargest([3,2,1,5,6,4], 2)); // 5
+console.log(findKthLargest([1], 1)); // 1
