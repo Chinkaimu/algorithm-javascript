6c29490fd7c588bf2e0c010f34c13e5774cc9fe0
chinkaimu <823388255@qq.com>
2020-04-02 11:58:05 +0800

    feat: solution for 215

--- type3-Sort/215KthLargestElementInAnArray.js
+++ type3-Sort/215KthLargestElementInAnArray.js
@@ -24,24 +24,26 @@ var findKthLargest = function(nums, k) {
       }
 
       if (left <= right) {
-        left < right && ([nums[left], nums[right]] = [nums[right], nums[left]]);
+        [nums[left], nums[right]] = [nums[right], nums[left]];
         left++;
         right--;
       }
     }
 
     // left 有可能超出界限
-    // 2 种指针存在情况： right pivotIndex left; right left(left 可能指向原来的 pivot)
-    if (right + 1 === k - 1) {
-      return nums[right + 1]
-    } else if ((k - 1) < left) {
+    // 2 种指针存在情况： right pivotIndex left; right left(left 可能指向原来的 pivot)。可能是 3 部分存在。
+    // 所以中间位置是 right + 1
+    if ((k - 1) <= right) {
       end = right;
-    } else {
+    } else if ((k - 1) >= left){
       start = left;
+    } else {
+      // 不在左边，不在右边，就是在中间。
+      return nums[right + 1];
     }
   }
 }
 
-console.log(findKthLargest([-1,2,0], 2));  // 0
-console.log(findKthLargest([3,2,1,5,6,4], 2)); // 5
-console.log(findKthLargest([1], 1)); // 1
+console.log(findKthLargest([-1, 2, 0], 2));  // 0 出错一次
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2)); // 5
+console.log(findKthLargest([1], 1));  // 1 出错一次
