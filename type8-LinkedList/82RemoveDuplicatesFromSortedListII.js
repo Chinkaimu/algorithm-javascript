c44d816b6218cf78cc594cae4dd889e7e45bcbac
chinkaimu <823388255@qq.com>
2020-04-06 20:51:57 +0800

    feat: solution for 82, second

--- type8-LinkedList/82RemoveDuplicatesFromSortedListII.js
+++ type8-LinkedList/82RemoveDuplicatesFromSortedListII.js
@@ -2,7 +2,7 @@
  * https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
  * 1-1-2-2-3-4-5-6-6-8
  * 3-4-8
- * 
+ * 考虑重复在前、中、后位置
  * Definition for singly-linked list.
  * function ListNode(val) {
  *     this.val = val;
@@ -13,6 +13,33 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
+var deleteDuplicates = function(head) {
+  if (!head || !head.next) return head
+
+  const dummyNode = new ListNode(0)
+  dummyNode.next = head
+  // Important
+  head = dummyNode
+
+  // 保留 3 个位置，后面 2 个位置进行数值比较，相等的话则一直往后走到不相等。
+  while (head && head.next && head.next.next) {
+    if (head.next.val === head.next.next.val) {
+      deleteItem = head.next.val
+
+      // head 点不变，next 内容一步步往后移动，直到找到不等于 deleteItem 的内容，接到 head 之后
+      while (head.next && head.next.val === deleteItem) {
+        head.next = head.next.next
+      }
+
+    } else {
+      head = head.next
+    }
+  }
+
+  return dummyNode.next
+};
+
 var deleteDuplicates = function(head) {
   const dummyNode = new ListNode();
   let preNode = dummyNode;
@@ -23,11 +50,17 @@ var deleteDuplicates = function(head) {
   while (curNode !== null) {
     if (curNode.next && curNode.next.val === curNode.val) {
       toDeleteValue = curNode.val;
-      curNode = curNode.next.next;
+      // TODO: 不要直接跳到下一个的下一个，因为下下个可能是 null
+      curNode = curNode.next;
       continue;
     }
     
     if (curNode.val === toDeleteValue) {
+      // TODO: 如果下一个是 null 的话，要直接拼接上
+      if (curNode.next === null) {
+        preNode.next = null;
+        break;
+      }
       curNode = curNode.next;
       continue;
     }
