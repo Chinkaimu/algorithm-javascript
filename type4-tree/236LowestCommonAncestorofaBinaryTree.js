0f5e31a0b69fc8264ef70d5c93d6585d8e91f8af
chinkaimu <823388255@qq.com>
2020-04-21 12:26:23 +0800

    fix: add comments

--- type4-tree/236LowestCommonAncestorofaBinaryTree.js
+++ type4-tree/236LowestCommonAncestorofaBinaryTree.js
@@ -5,7 +5,8 @@
  *    可以 p/q 节点一层层向上走，相遇的地方就是 LCA。
  *    left --> p, right is ancestor to q, then the root is the ancestor of p,q
  * Divide & conquer
- *      dfs 找到 p 或 q 或者 null 不存在 就 return 这 3 个值
+ *      【返回值】dfs 找到 p 或 q 或者 null 不存在 就 return 这 3 个值;
+ *      【结果合并】左右都不是 null，则 root 就是 LCA； 左边或右边一个 null，一个非 null，则返回非 null 值。
  * 
  * Definition for a binary tree node.
  * function TreeNode(val) {
